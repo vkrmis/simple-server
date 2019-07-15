28702dbbbfd01426de75832d728b6a3408f12774
vkrm <mail@vkrm.is>
2019-07-15 13:27:55 +0530

    add whitelist details api call

--- bin/one-off/exotel_script.clj
+++ bin/one-off/exotel_script.clj
@@ -3,26 +3,39 @@
 (use '[leiningen.exec :only (deps)])
 (deps '[[clj-http "3.10.0"]])
 (require '[clj-http.client :as client])
-(require '[clojure.xml :as xml])           
+(require '[clojure.xml :as xml])
 
 (def sid "resolvetosavelives")
 (def token "5b8ed10d2fe4745a7d9c9d09d7f156c47ea2eca8")
 
-(def metadata-url "api.exotel.com/v1/Accounts/resolvetosavelives/Numbers/")
+(def metadata-url (str "https://" sid ":" token "@api.exotel.com/v1/Accounts/" sid "/Numbers/"))
+(def whitelist-details-url (str "https://" sid ":" token "@api.exotel.com/v1/Accounts/" sid "/CustomerWhitelist/"))
 
-(defn phone-number-attributes-from-response [response]
+(defn metadata-from-response [response]
   (let [parsed-metadata (group-by :tag (get-in response [:content 0 :content]))]
     {:phone-number  (get-in parsed-metadata [:PhoneNumber 0 :content 0])
      :dnd           (get-in parsed-metadata [:DND 0 :content 0])
      :type          (get-in parsed-metadata [:Type 0 :content 0])}))
- 
+
+(defn whitelist-details-from-response [response]
+  (let [parsed-metadata (group-by :tag (get-in response [:content 0 :content]))]
+    {:status  (get-in parsed-metadata [:Status 0 :content 0])
+     :expiry  (get-in parsed-metadata [:Expiry 0 :content 0])
+     :type    (get-in parsed-metadata [:Type 0 :content 0])}))
+
 (defn get-metadata [number]
   (-> number
-   (#(client/get (str "https://" sid ":" token "@" metadata-url %)))
-   (#(xml/parse (java.io.ByteArrayInputStream. (.getBytes (:body %)))))
-   (#(phone-number-attributes-from-response %))))
+      (#(client/get (str metadata-url %)))
+      (#(xml/parse (java.io.ByteArrayInputStream. (.getBytes (:body %)))))
+      (#(metadata-from-response %))))
 
-(do 
+(defn get-whitelist-details [number]
+  (-> number
+      (#(client/get (str whitelist-details-url %)))
+      (#(xml/parse (java.io.ByteArrayInputStream. (.getBytes (:body %)))))
+      (#(whitelist-details-from-response %))))
+
+
+(do
   (let [numbers (rest *command-line-args*)]
-    (println (get-metadata (first numbers)))))
-
+    (println (get-whitelist-details (first numbers)))))
